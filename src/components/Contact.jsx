d1fbb00ae0364754dadf3854a1552e2ca428f751
wojtek-d <drabczykw1@gmail.com>
2023-02-22 18:53:10 +0100

    contact.jsx not working

---
+++ src/components/Contact.jsx
@@ -0,0 +1,8 @@
+export const NavBar = () => {
+  return (
+    <nav>
+      <button type="button">Email</button>
+      <button type="button">LinkedIn</button>
+    </nav>
+  );
+};
---
+++ src/components/MyApp.jsx
@@ -0,0 +1,9 @@
+import { Contact } from "./Contact";
+
+export const MyApp = () => {
+  return (
+    <>
+      <Contact />
+    </>
+  );
+};

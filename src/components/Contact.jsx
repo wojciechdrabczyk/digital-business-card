181ab81ee517c0ed9f10feb72a6f00cb11866501
wojtek-d <drabczykw1@gmail.com>
2023-02-23 18:20:34 +0100

    pls fix css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,26 @@
 export const Contact = () => {
   return (
-    <div className="buttons">
-      <button className="EmailButton">
-        <div className="btn-content-center-email">
-          <img src="../images/EmailLogo.png" alt="Email logo" />
+    <>
+      <button className="btn">
+        <div className="btn-content">
+          <img
+            src="../images/EmailLogo.png"
+            alt="Email logo"
+            style={{ width: "15px" }}
+          />
           <span>Email</span>
         </div>
       </button>
-      <button className="LinkedInButton">
-        <div className="btn-content-center-linkedin">
-          <img src="../images/LinkedInLogo.png" alt="LinkedIn logo" />
+      <button className="btn btn--linkedin">
+        <div className="btn-content">
+          <img
+            src="../images/LinkedInLogo.png"
+            alt="LinkedIn logo"
+            style={{ width: "15px" }}
+          />
           <span>LinkedIn</span>
         </div>
       </button>
-    </div>
+    </>
   );
 };
